a1584c4e7a4a71da01182b6388da7974c9c458e6
hyunkeun-park <hermes7308@gmail.com>
2019-07-16 02:26:34 -0700

    중간 저장

--- app/src/main/java/com/hermes/camerabook/SelectActivity.java
+++ app/src/main/java/com/hermes/camerabook/SelectActivity.java
@@ -1,10 +1,16 @@
 package com.hermes.camerabook;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.ads.AdListener;
@@ -14,8 +20,13 @@ import com.google.android.gms.ads.MobileAds;
 import com.scanlibrary.ScanActivity;
 import com.scanlibrary.ScanConstants;
 
+import java.lang.reflect.Method;
+
 public class SelectActivity extends AppCompatActivity {
     private static final String TAG = SelectActivity.class.getName();
+    public static final String DISABLE_DEATH_ON_FILE_URI_EXPOSURE = "disableDeathOnFileUriExposure";
+
+    private AlertDialog selectMediaDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +41,31 @@ public class SelectActivity extends AppCompatActivity {
         mAdView.setAdListener(new AdListener());
 
         // View
-        findViewById(R.id.camera_area).setOnClickListener(onTouchListener);
-        findViewById(R.id.gallery_area).setOnClickListener(onTouchListener);
+        findViewById(R.id.addNewPage).setOnClickListener(onClickListener);
+        findViewById(R.id.openCapturedPage).setOnClickListener(onClickListener);
+
+        LayoutInflater inflater = getLayoutInflater();
+        View selectMediaPopup = inflater.inflate(R.layout.select_media_popup, null);
+        selectMediaPopup.findViewById(R.id.select_media_popup_camera)
+                .setOnClickListener(selectMediaOnClickListener);
+        selectMediaPopup.findViewById(R.id.select_media_popup_picture)
+                .setOnClickListener(selectMediaOnClickListener);
+
+        selectMediaDialog = new AlertDialog.Builder(this)
+                .setView(selectMediaPopup)
+                .setCancelable(true)
+                .create();
     }
 
-    private View.OnClickListener onTouchListener = new View.OnClickListener() {
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             switch (view.getId()) {
-                case R.id.camera_area:
-                    openCamera();
+                case R.id.addNewPage:
+                    selectMediaDialog.show();
                     break;
-                case R.id.gallery_area:
-                    openGallery();
+                case R.id.openCapturedPage:
+                    openCapturedPage();
                     break;
                 default:
                     break;
@@ -56,10 +79,22 @@ public class SelectActivity extends AppCompatActivity {
         startActivityForResult(intent, RequestCode.CAMERA.getCode());
     }
 
-    private void openGallery() {
+    private void openMedia() {
         Intent intent = new Intent(this, ScanActivity.class);
         intent.putExtra(ScanConstants.OPEN_INTENT_PREFERENCE, ScanConstants.OPEN_MEDIA);
-        startActivityForResult(intent, RequestCode.GALLERY.getCode());
+        startActivityForResult(intent, RequestCode.MEDIA.getCode());
+    }
+
+    private void openCapturedPage() {
+        final Intent galleryIntent = new Intent(Intent.ACTION_PICK);
+        galleryIntent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(galleryIntent,  RequestCode.CAPTURED_PAGE.getCode());
+    }
+
+    private void openTextDetection(String imagePath) {
+        Intent intent = new Intent(this, TextDetectionActivity.class);
+        intent.putExtra(TextDetectionActivity.CAPTURE_IMAGE_PATH_KEY, imagePath);
+        startActivity(intent);
     }
 
     @Override
@@ -68,18 +103,38 @@ public class SelectActivity extends AppCompatActivity {
             return;
         }
 
-        if (requestCode == RequestCode.CAMERA.getCode() || requestCode == RequestCode.GALLERY.getCode()) {
+        if (requestCode == RequestCode.CAMERA.getCode() || requestCode == RequestCode.MEDIA.getCode()) {
             String captureImagePath = data.getExtras().getString(ScanConstants.SCANNED_RESULT);
+            openTextDetection(captureImagePath);
+            return;
+        }
 
-            Intent intent = new Intent(this, TextDetectionActivity.class);
-            intent.putExtra(TextDetectionActivity.CAPTURE_IMAGE_PATH_KEY, captureImagePath);
-            startActivity(intent);
+        if (requestCode == RequestCode.CAPTURED_PAGE.getCode()) {
+            Uri uri = data.getData();
+            String imagePath = uri.getPath();
+            openTextDetection(imagePath);
+            return;
         }
     }
 
+    private View.OnClickListener selectMediaOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.select_media_popup_camera:
+                    openCamera();
+                    break;
+                case R.id.select_media_popup_picture:
+                    openMedia();
+                    break;
+            }
+        }
+    };
+
     enum RequestCode {
         CAMERA(1),
-        GALLERY(2);
+        MEDIA(2),
+        CAPTURED_PAGE(3);
 
         private int code;
 
